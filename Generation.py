2af9cb99f11e62562a263654f40287cb0602336c
parshas <shachar.parshani@gamil.com>
2023-06-08 16:24:41 +0300

    Initial commit

---
+++ Generation.py
@@ -0,0 +1,20 @@
+from Permutation import Permutation
+class Generation:
+    def __init__(self, n, ):
+        self.n = n  # n- size of population
+        self.generation = []
+
+    def create_first_generation(self):
+        for i in range(self.n):
+            p = Permutation()
+            p.upgrade_fitness()
+            self.generation.append(p)
+
+    def order_by_fitness(self): # high to low
+        def fit(p):
+            return p.fitness
+        return self.generation.sort(reverse=True, key=fit)
+
+
+
+
---
+++ global_processes.py
@@ -0,0 +1,6 @@
+from global_processes import ACTUAL_CLASS
+from global_processes import TEST_CLASS
+from global_processes import TRAIN_STR
+from global_processes import TEST_STR
+
+NUM_WEIGHT = 350  #TODO: changee!!!!!!!
